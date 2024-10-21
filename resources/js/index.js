8b36509d38d26182e1bf0fd4693e5aff7ecea761
mythic3011 <me@mythic3011.xyz>
2024-10-21 22:14:59 +0800

    refactor the icon image path

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,77 +1,85 @@
-    $(document).ready(function () {
-        $('#loginForm').on('submit', function (event) {
-            event.preventDefault(); // Prevent form submission
+$(document).ready(function () {
+    $('#loginForm').on('submit', function (event) {
+        event.preventDefault(); // Prevent form submission
 
-            const username = $('#username').val().trim();
-            const password = $('#password').val().trim();
-            const $loginButton = $('button[type="submit"]');
-            const $message = $('#message');
-            const $systemSelect = $('#system-select-form').val();
+        const username = $('#username').val().trim();
+        const password = $('#password').val().trim();
+        const $loginButton = $('button[type="submit"]');
+        const $message = $('#message');
+        const $systemSelect = $('#system-select-form').val();
 
-            // Show loading spinner
-            $loginButton.addClass('loading').prop('disabled', true);
+        // Show loading spinner
+        $loginButton.addClass('loading').prop('disabled', true);
 
-            $.getJSON('resources/json/user.json', function (users) {
-                const user = users.find(user => user.email === username && user.password === password);
+        $.getJSON('resources/json/user.json', function (users) {
+            const user = users.find(user => user.email === username && user.password === password);
 
-                if (user) {
-                    $message.text('Login successful! Redirecting...').addClass('success').removeClass('error');
-                    $loginButton.removeClass('loading').text('Login successful!');
+            if (user) {
+                $message.text('Login successful! Redirecting...').addClass('success').removeClass('error');
+                $loginButton.removeClass('loading').text('Login successful!');
 
-                    if ($('#remember-me').is(':checked')) {
-                        localStorage.setItem('username', username);
-                    } else {
-                        localStorage.removeItem('username');
-                    }
-
-                    // Store the user data in the session storage
-                    sessionStorage.setItem('user', JSON.stringify(user));
-                    sessionStorage.setItem('login-system', $systemSelect);
-                    sessionStorage.setItem('logout', false);
-
-                    // Redirect based on user role
-                    setTimeout(() => {
-                        let post = `${user.post === "customer" ? "N" : user.post}`;
-                        window.location.href = `./page/${user.role}/${$systemSelect}/index.html`;
-                    }, 3000);
+                if ($('#remember-me').is(':checked')) {
+                    localStorage.setItem('username', username);
                 } else {
-                    $message.text('Invalid username or password.').addClass('error').removeClass('success');
-                    $loginButton.removeClass('loading').prop('disabled', false).text('Login');
+                    localStorage.removeItem('username');
                 }
-            }).fail(function () {
-                $message.text('Error loading user data.').addClass('error').removeClass('success');
+
+                // Store the user data in the session storage
+                sessionStorage.setItem('user', JSON.stringify(user));
+                sessionStorage.setItem('login-system', $systemSelect);
+                sessionStorage.setItem('logout', false);
+
+                // Redirect based on user role
+                setTimeout(() => {
+                    let post = `${user.post === "customer" ? "N" : user.post}`;
+                    window.location.href = `./page/${user.role}/${$systemSelect}/index.html`;
+                }, 3000);
+            } else {
+                $message.text('Invalid username or password.').addClass('error').removeClass('success');
                 $loginButton.removeClass('loading').prop('disabled', false).text('Login');
-            });
+            }
+        }).fail(function () {
+            $message.text('Error loading user data.').addClass('error').removeClass('success');
+            $loginButton.removeClass('loading').prop('disabled', false).text('Login');
         });
-
-        // Load remembered username
-        const rememberedUsername = localStorage.getItem('username');
-        if (rememberedUsername) {
-            $('#username').val(rememberedUsername);
-            $('#remember-me').prop('checked', true);
-        }
-
-        $('#darkModeToggle').on('click', function () {
-            $('body').toggleClass('dark-mode');
-            $('.container').toggleClass('dark-mode');
-            $('input').toggleClass('dark-mode');
-            $('#darkModeToggle').toggleClass('dark-mode');
-            $('#switch-theme').toggleClass('dark-mode');
-            $('footer').toggleClass('dark-mode');
-
-            // Save theme preference
-            const isDarkMode = $('body').hasClass('dark-mode');
-            localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-        });
-
-        // Check system preference for dark mode or saved preference
-        const savedTheme = localStorage.getItem('theme');
-        if (savedTheme === 'dark' || (!savedTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            $('body').addClass('dark-mode');
-            $('.container').addClass('dark-mode');
-            $('input').addClass('dark-mode');
-            $('#darkModeToggle').addClass('dark-mode');
-            $('body').addClass('switch-theme');
-            $('footer').addClass('dark-mode');
-        }
     });
+
+    // Load remembered username
+    const rememberedUsername = localStorage.getItem('username');
+    if (rememberedUsername) {
+        $('#username').val(rememberedUsername);
+        $('#remember-me').prop('checked', true);
+    }
+
+    $('#darkModeToggle').on('click', function () {
+        $('body').toggleClass('dark-mode');
+        $('.container').toggleClass('dark-mode');
+        $('input').toggleClass('dark-mode');
+        $('#darkModeToggle').toggleClass('dark-mode');
+        $('#switch-theme').toggleClass('dark-mode');
+        $('footer').toggleClass('dark-mode');
+
+        // Save theme preference
+        const isDarkMode = $('body').hasClass('dark-mode');
+        localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+    });
+
+    // Check system preference for dark mode or saved preference
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark' || (!savedTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        $('body').addClass('dark-mode');
+        $('.container').addClass('dark-mode');
+        $('input').addClass('dark-mode');
+        $('#darkModeToggle').addClass('dark-mode');
+        $('body').addClass('switch-theme');
+        $('footer').addClass('dark-mode');
+    }
+
+    const isLoggedIn = sessionStorage.getItem('logout') === false;
+
+    // Check if the user is logged in
+    if (isLoggedIn) {
+        // redirect to user role page and system
+        window.location.href = `./page/${sessionStorage.getItem('user').role}/${sessionStorage.getItem('login-system')}/index.html`;
+    }
+});
--- resources/js/script.js
+++ resources/js/script.js
@@ -1,29 +1,12 @@
-// main.js
-import { logout } from './module/auth.js';
-import { toggleDarkMode, setInitialDarkMode } from './module/darkMode.js';
-import { updatePageLanguage } from './module/language.js';
+// script.js
 
 document.addEventListener('DOMContentLoaded', function() {
-    const $ = id => document.getElementById(id);
     // Dark mode
-    setInitialDarkMode();
-    $('darkModeToggle').addEventListener('click', toggleDarkMode);
+    const isLoggedIn = sessionStorage.getItem('logout') === false;
 
-    // Mobile menu
-    const mobileMenuButton = $('mobile-menu-button');
-    const navbar = $('navbar-default');
-
-    function toggleMobileMenu() {
-        navbar.classList.toggle('hidden');
+    // Check if the user is logged in
+    if (!isLoggedIn) {
+        // redirect to login page
+        window.location.href = '../index.html';
     }
-
-    mobileMenuButton.addEventListener('click', toggleMobileMenu);
-
-    // Logout functionality
-    $('logoutButton').addEventListener('click', logout);
-
-    // Store the current page in the history array
-    const historyArr = JSON.parse(sessionStorage.getItem('pageHistory')) || [];
-    historyArr.push(window.location.href);
-    sessionStorage.setItem('pageHistory', JSON.stringify(historyArr));
 });
